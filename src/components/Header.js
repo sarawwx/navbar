97a9dc65a529588a2859163a495dbb7b84923b3e
Sara L <wwwsaramilk@gmail.com>
2022-06-03 00:04:34 -0300

    comecando header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,28 @@ import React from 'react';
 import classes from './Header.module.scss';
 
 const Header = () => {
-  return <header className={classes.header}></header>;
+  return <header className={classes.header}>
+      <div className={classes.header__content}>
+        <h2>navbar</h2>
+      </div>
+      <nav className={classes.header__content__nav}>
+          <ul>
+              <li>
+                  <a href='/'>PageOne</a>
+              </li>
+              <li>
+                  <a href='/'>PageTwo</a>
+              </li>
+              <li>
+                  <a href='/'>PageThree</a>
+              </li>
+          </ul>
+          <button>CTA Page</button>
+      </nav>
+      <div className={classes.header__content_toggle}>
+          
+      </div>
+  </header>;
 }
 
 export default Header
