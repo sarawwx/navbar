6cd7c4df98c049f7d0ff3d9dc21b05403087bb5c
Sara L <wwwsaramilk@gmail.com>
2022-06-07 22:01:01 -0300

    adicionando itens ao header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,25 @@ import styles from './Header.module.scss';
 const Header = () => {
   return (
     <div className={styles.header}>
-        <div className={styles.header__content}>hi</div>
+        <div className={styles.header__content}>
+            <div>
+                <span className={styles.logo}>La la land</span>
+            </div>
+            <div>
+                <nav className={styles.nav}>
+                    <a className={styles.nav__item} href={"./"}> 
+                        Page One
+                    </a>
+                    <a className={styles.nav__item} href={"./"}> 
+                        Page Two
+                    </a>
+                    <a className={styles.nav__item} href={"./"}> 
+                        Page Three
+                    </a>
+                </nav>
+            </div>
+            <div></div>
+        </div>
     </div>
   )
 }
